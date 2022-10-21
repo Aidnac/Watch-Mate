71d853bb66d4c22ccf185da4d9d5ce3f5131981a
Can-A <calex6658@gmail.com>
2022-10-21 15:35:19 +0300

    single Detail using Django only not django rest framework

--- watchlist_app/views.py
+++ watchlist_app/views.py
@@ -9,5 +9,13 @@ def movie_list(request):
     data = {
         'movies': list(movies.values())
     }
+    return JsonResponse(data)
 
+
+def movie_details(request, pk):
+    movie = Movie.objects.get(pk=pk)
+    data = {
+        'name': movie.name,
+        'description': movie.description
+    }
     return JsonResponse(data)
