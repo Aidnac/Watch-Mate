76fbec12c41025b71c0c66925f742b1f20442a33
Can-A <calex6658@gmail.com>
2022-10-22 23:00:07 +0300

    serializer - get request

--- watchlist_app/views.py
+++ watchlist_app/views.py
@@ -1,21 +1,21 @@
-from django.http import JsonResponse
-
-from watchlist_app.models import Movie
-
-
-# Create your views here.
-def movie_list(request):
-    movies = Movie.objects.all()
-    data = {
-        'movies': list(movies.values())
-    }
-    return JsonResponse(data)
-
-
-def movie_details(request, pk):
-    movie = Movie.objects.get(pk=pk)
-    data = {
-        'name': movie.name,
-        'description': movie.description
-    }
-    return JsonResponse(data)
+# from django.http import JsonResponse
+#
+# from watchlist_app.models import Movie
+#
+#
+# # Create your views here.
+# def movie_list(request):
+#     movies = Movie.objects.all()
+#     data = {
+#         'movies': list(movies.values())
+#     }
+#     return JsonResponse(data)
+#
+#
+# def movie_details(request, pk):
+#     movie = Movie.objects.get(pk=pk)
+#     data = {
+#         'name': movie.name,
+#         'description': movie.description
+#     }
+#     return JsonResponse(data)
